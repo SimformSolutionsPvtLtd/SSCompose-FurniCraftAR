c7e86e09db3f6a8370feb5a85bd255ce99afc8cc
Krunal Patel <krunal.patel@simformsolutions.com>
2024-04-01 13:24:21 +0530

    UNT-T23347: Add network observer
    - Notify user when the internet connection is changed (i.e. not available)

--- app/src/main/java/com/simform/ssfurnicraftar/SSFurniCraftARApplication.kt
+++ app/src/main/java/com/simform/ssfurnicraftar/SSFurniCraftARApplication.kt
@@ -1,24 +1,16 @@
 package com.simform.ssfurnicraftar
 
 import android.app.Application
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
-import com.simform.ssfurnicraftar.utils.NetworkUtil
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class SSFurniCraftARApplication : Application() {
 
-    private val validNetworks: MutableList<Network> = mutableListOf()
-
     override fun onCreate() {
         super.onCreate()
 
         initTimber()
-        observeNetwork()
     }
 
     private fun initTimber() {
@@ -27,47 +19,4 @@ class SSFurniCraftARApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
-
-    /**
-     * Observe network state.
-     */
-    private fun observeNetwork() {
-        val connectivityManager: ConnectivityManager =
-            getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkRequest = NetworkRequest.Builder()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-            .build()
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (!hasInternet(capabilities)) {
-            checkValidNetworks()
-        }
-        connectivityManager.registerNetworkCallback(networkRequest, object :
-            ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
-                if (hasInternet(networkCapabilities)) {
-                    validNetworks.add(network)
-                    checkValidNetworks()
-                }
-            }
-
-            override fun onLost(network: Network) {
-                validNetworks.remove(network)
-                checkValidNetworks()
-            }
-        })
-    }
-
-    private fun checkValidNetworks() {
-        NetworkUtil.isNetworkConnected = validNetworks.isNotEmpty()
-    }
-
-    private fun hasInternet(networkCapabilities: NetworkCapabilities?): Boolean =
-        networkCapabilities != null &&
-                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
 }
--- app/src/main/java/com/simform/ssfurnicraftar/ui/MainActivity.kt
+++ app/src/main/java/com/simform/ssfurnicraftar/ui/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.simform.ssfurnicraftar.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.simform.ssfurnicraftar.navigation.AppNavHost
+import com.simform.ssfurnicraftar.data.utils.NetworkMonitor
 import com.simform.ssfurnicraftar.ui.theme.SSFurniCraftARTheme
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val appState = rememberAppState(
+                networkMonitor = networkMonitor
+            )
+
             SSFurniCraftARTheme {
-                SSFurniCraftARApp()
+                SSFurniCraftARApp(appState)
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Timber.d("intent: ${intent == null}")
     }
 }
